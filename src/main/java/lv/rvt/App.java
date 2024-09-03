f3dd19288970bc52a83993385ec55a2f633c71bc
23DP1EMozg <166118194+23DP1EMozg@users.noreply.github.com>
2024-09-03 09:44:05 +0000

    name age online

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,7 +7,17 @@ public class App
 {
     public static void main( String[] args )
     {
-       System.out.println("hello world");
+       //my code starts here 
+       String name = "edvards";
+       String surname = "megnis";
+       int age = 17;
+       boolean online = false;
+
+       System.out.println(name);
+       System.out.println(surname);
+       System.out.println(age);
+       System.out.println(online);
+
 
     }
 

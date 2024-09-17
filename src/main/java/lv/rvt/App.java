abfc989e12bf89aee39c6403b9c496c0d1133027
23DP1EMozg <166118194+23DP1EMozg@users.noreply.github.com>
2024-09-17 09:32:45 +0000

    Larger Than or Equal To

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,15 +4,21 @@ import java.util.Scanner;
 public class App
 {
 public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+   Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Give speed:");
-    int speed = Integer.valueOf(scanner.nextLine());
+   System.out.println("Give the first number: ");
+   int num1 = Integer.valueOf(scanner.nextLine());
+   System.out.println("Give the second number: ");
+   int num2 = Integer.valueOf(scanner.nextLine());
 
-    if (speed > 120) {
-         System.out.println("Speeding ticket!");
-    }
+   if(num1 > num2){
+    System.out.println("Greater number is: " + num1);
+   }else if(num2 > num1){
+    System.out.println("Greater number is: " + num2);
+   }else{
+    System.out.println("The numbers are equal!");
+   }
 
-    scanner.close();
+   scanner.close();
 }
 }

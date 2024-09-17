00d65650e33bed0810342119b93362c90c14b50e
23DP1EMozg <166118194+23DP1EMozg@users.noreply.github.com>
2024-09-17 07:55:29 +0000

    speeding ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,31 +1,18 @@
 package lv.rvt;
-
-
 import java.util.Scanner;
 
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        Scanner scanner = new Scanner(System.in);
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.print("name: ");
-        String name = scanner.nextLine();
+    System.out.println("Give speed:");
+    int speed = Integer.valueOf(scanner.nextLine());
 
-        System.out.print("last name: ");
-        String lastname = scanner.nextLine();
-
-        System.out.print("grupa: ");
-        String group = scanner.nextLine();
-
-        System.out.println();
-
-        System.out.println("your name is " + name + ", your last name is " + lastname + " and your group is " + group);
-
-
-        scanner.close();
-
-
+    if (speed > 120) {
+         System.out.println("Speeding ticket!");
     }
 
+    scanner.close();
+}
 }

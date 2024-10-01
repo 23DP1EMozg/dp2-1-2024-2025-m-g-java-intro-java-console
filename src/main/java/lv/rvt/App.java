742257d5db1de5a2d345c80fa68b0e0805737dd9
23DP1EMozg <166118194+23DP1EMozg@users.noreply.github.com>
2024-10-01 09:19:55 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,14 +6,16 @@ public class App
    public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
     
-      printUntilNumber(5);
+      divisibleByThreeInRange(2, 10);
       scanner.close();
    }
 
 
-   public static void printUntilNumber(int n){
-      for(int i = 1; i<=n; i++){
-         System.out.println(i);
+   public static void divisibleByThreeInRange(int num1, int num2){
+      for(int i = num1; i<=num2; i++){
+         if(i % 3 == 0){
+            System.out.println(i);
+         }
       }
    }
 }
